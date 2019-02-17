1f92eb2ee590ab2860bef16173d7f7d2a202561d
tzk007 <751824405@qq.com>
2019-02-17 12:36:53 +0800

    Initial commit

---
+++ hikariCP/src/main/java/com/learing/hikariCP/SpringBootHikariCpApplication.java
@@ -0,0 +1,12 @@
+package com.learing.hikariCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHikariCpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHikariCpApplication.class, args);
+    }
+}
